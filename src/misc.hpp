9c20c4e5f261c8e627f9a8565a7e3ec7f31cdfd3
Nishiyama Nobuyuki <tek.nishi@gmail.com>
2016-06-07 17:38:02 +0900

    Win:UTF8toCP932のメモリリークを修正

--- src/misc.hpp
+++ src/misc.hpp
@@ -10,24 +10,24 @@
 using u_int = unsigned int;
 
 // UTF-8 → cp932
-std::string UTF8toCP932(std::string srcUTF8) {
-	// Unicodeへ変換後の文字列長を得る
-	int lengthUnicode = MultiByteToWideChar(CP_UTF8, 0, srcUTF8.c_str(), srcUTF8.size(), nullptr, 0);
+std::string UTF8toCP932(const std::string& srcUTF8) {
+  // Unicodeへ変換後の文字列長を得る
+  int lengthUnicode = MultiByteToWideChar(CP_UTF8, 0, srcUTF8.c_str(), srcUTF8.size(), nullptr, 0);
 
-	// UTF8からUnicodeへ変換
-	std::vector<wchar_t> bufUnicode(lengthUnicode);
-	MultiByteToWideChar(CP_UTF8, 0, srcUTF8.c_str(), srcUTF8.size(), &bufUnicode[0], lengthUnicode);
+  // UTF8からUnicodeへ変換
+  std::vector<wchar_t> bufUnicode(lengthUnicode);
+  MultiByteToWideChar(CP_UTF8, 0, srcUTF8.c_str(), srcUTF8.size(), &bufUnicode[0], lengthUnicode);
 
-	// CP932へ変換後の文字列長を得る
-	int lengthCP932 = WideCharToMultiByte(CP_THREAD_ACP, 0, &bufUnicode[0], -1, nullptr, 0, nullptr, nullptr);
+  // CP932へ変換後の文字列長を得る
+  int lengthCP932 = WideCharToMultiByte(CP_THREAD_ACP, 0, &bufUnicode[0], lengthUnicode, nullptr, 0, nullptr, nullptr);
 
-	// UnicodeからShiftJISへ変換
-	std::vector<char> bufCP932(lengthCP932);
-	WideCharToMultiByte(CP_THREAD_ACP, 0, &bufUnicode[0], lengthUnicode, &bufCP932[0], lengthCP932, nullptr, nullptr);
+  // UnicodeからShiftJISへ変換
+  std::vector<char> bufCP932(lengthCP932);
+  WideCharToMultiByte(CP_THREAD_ACP, 0, &bufUnicode[0], lengthUnicode, &bufCP932[0], lengthCP932, nullptr, nullptr);
 
-	std::string dstCP932(bufCP932.begin(), bufCP932.end());
+  std::string dstCP932(bufCP932.begin(), bufCP932.end());
 
-	return dstCP932;
+  return dstCP932;
 }
 
 #endif
